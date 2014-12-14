689910cfdb47c58944047e0280db660a0f67c280
Stephane Bounmy <stephanebounmy@gmail.com>
2014-12-14 22:18:26 +0100

    disable deploys true

--- lib/language_pack/disable_deploys.rb
+++ lib/language_pack/disable_deploys.rb
@@ -3,7 +3,8 @@ require "language_pack/base"
 
 class LanguagePack::DisableDeploys < LanguagePack::Base
   def self.use?
-    File.exist?("Gemfile")
+    true
+    # File.exist?("Gemfile")
   end
 
   def name
